abb86ea141316e8ab227599918db9f067958d255
SandovalAlexis <sebasandoval100@gmail.com>
2021-12-08 20:17:49 -0300

    creamos una pagina de inicio

--- market/market/settings.py
+++ market/market/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
