a0b69175cb2f7d0222844b0d6aeda480fd68b02d
SandovalAlexis <sebasandoval100@gmail.com>
2021-12-13 19:43:54 -0300

    agregamos administrador de registro

--- market/market/views.py
+++ market/market/views.py
@@ -3,16 +3,8 @@ from django.shortcuts import render
 from apps.productos.models import Producto
 
 def inicio(request):
-    producto = Producto.objects.all()
-    
-    usuario = {
-        "nombre" : "sebastian",
-        "apellido" : "sandoval"
-    }
-    
     context = {
-        "usuario" : usuario,
-        "producto" : producto
+        "producto" : Producto.objects.all()
     }
     return render(request, "inicio.html", context)
 
